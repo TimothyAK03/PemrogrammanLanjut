45f95aa130bbefcd90787b1a4ae53f630c2fa740
Timothy Arif Kurniawan <108077845+TimothyAK03@users.noreply.github.com>
2022-10-17 00:36:34 +0700

    Tugas2PLBankWeb
    Tugas 2 Pemrogramman Lanjut Bank Web  
    Mario Indra Widjaja Hindarto / 672020013  
    Timothy Arif Kurniawan / 672020104

---
+++ Tugas2BankWeb/src/java/Dao/AtmDao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+
+/**
+ *
+ * @author Anggasta
+ */
+public interface AtmDao {
+    int lihatSaldo();
+    
+    int cekProses(int jumlah);
+    
+    boolean cekBatasSaldo();
+    
+    void setorUang(int jumlah);
+    
+    void tarikUang(int jumlah);
+    
+}                               
